fd856fe55b59d0d075d626e7004fd4d317605fdc
Simeon052 <simeon_the_city_who_fought@outlook.com>
2017-08-19 21:06:24 +0900

    Minimum client server connection

--- Cmd2WebApp/server.js
+++ Cmd2WebApp/server.js
@@ -1,8 +1,22 @@
 'use strict';
+var fs = require('fs');
 var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    var output = fs.readFileSync("./index.html", "utf-8");
+    res.end(output);
 }).listen(port);
+var io = require("socket.io").listen(server);
+
+io.sockets.on("connection", function (socket) {
+
+    socket.on("connected", function (value) {
+
+    });
+
+    socket.on("execOnServer", function (data) {
+        io.sockets.emit("ServerMsg", { value: "Message from Server : " + data.value });
+    });
+});
