344a862073e12c50178e5c3b28d0a98dcdab1109
Simeon052 <simeon_the_city_who_fought@outlook.com>
2017-08-19 23:35:18 +0900

    Set UTF-8 encoding

--- Cmd2Web/Program.cs
+++ Cmd2Web/Program.cs
@@ -11,9 +11,10 @@ namespace Cmd2Web
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = new UTF8Encoding();
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"Message from Command line : {i}");
+                Console.WriteLine($"Message from Command line テスト : {i}");
                 Thread.Sleep(1000);
             }
         }
