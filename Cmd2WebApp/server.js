0ab3829e00d9317dc743ded78f85d934a9b92e5c
Simeon052 <simeon_the_city_who_fought@outlook.com>
2017-08-19 21:46:36 +0900

    Add exec

--- Cmd2WebApp/server.js
+++ Cmd2WebApp/server.js
@@ -2,6 +2,7 @@
 var fs = require('fs');
 var http = require('http');
 var port = process.env.PORT || 1337;
+var exec = require('child_process').exec;
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -17,6 +18,15 @@ io.sockets.on("connection", function (socket) {
     });
 
     socket.on("execOnServer", function (data) {
+        exec('dir', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            io.sockets.emit("ServerMsg", { value: stdout.toString() });
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        });
         io.sockets.emit("ServerMsg", { value: "Message from client via Server : " + data });
     });
 });
