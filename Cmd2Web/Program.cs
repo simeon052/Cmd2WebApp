7c79400cedcb2a74eee1205dd98d2fa9a1c4a70b
Simeon052 <simeon_the_city_who_fought@outlook.com>
2017-08-20 12:08:38 +0900

    return image from command line

--- Cmd2Web/Program.cs
+++ Cmd2Web/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -12,10 +13,12 @@ namespace Cmd2Web
         static void Main(string[] args)
         {
             Console.OutputEncoding = new UTF8Encoding();
-            for (int i = 0; i < 10; i++)
+            var di = new DirectoryInfo(@".\images");
+            foreach(var f in di.GetFiles("*.jpg", SearchOption.TopDirectoryOnly))
             {
-                Console.WriteLine($"Message from Command line テスト : {i}");
-                Thread.Sleep(1000);
+                //                Console.WriteLine($"Message from command line :  {i} - {args[0] ?? ""}");
+                Console.WriteLine($"<a href = \"images\\{f.Name}\" >{f.Name}</a>");
+                Thread.Sleep(500);
             }
         }
     }
--- Cmd2WebApp/server.js
+++ Cmd2WebApp/server.js
@@ -3,12 +3,12 @@ var fs = require('fs');
 var http = require('http');
 var port = process.env.PORT || 1337;
 var spawn = require('child_process').spawn;
+var express = require('express');
+var routes = require
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    var output = fs.readFileSync("./index.html", "utf-8");
-    res.end(output);
-}).listen(port);
+var app = express();
+var server = http.createServer(app).listen(port);
+
 var io = require("socket.io").listen(server);
 
 io.sockets.on("connection", function (socket) {
@@ -18,7 +18,7 @@ io.sockets.on("connection", function (socket) {
     });
 
     socket.on("execOnServer", function (data) {
-        var execOut = spawn('..\\Cmd2Web\\bin\\Debug\\Cmd2Web.exe');
+        var execOut = spawn('..\\Cmd2Web\\bin\\Debug\\Cmd2Web.exe', [ data.toString("utf-8") ]);
 
         execOut.stdout.on('data', function (data) {
             io.sockets.emit("ServerMsg", { value: data.toString("utf-8") });
@@ -33,3 +33,27 @@ io.sockets.on("connection", function (socket) {
         io.sockets.emit("ServerMsg", { value: "Message from client via Server : " + data });
     });
 });
+
+app.get('/', function (req, res) {
+    fs.readFile('./index.html', 'UTF-8', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+});
+
+app.get('*.jpg', function (req, res) {
+    console.log(req);
+    fs.readFile('.' + req.originalUrl,function(err, data){  
+      res.set('Content-Type', 'image/jpg'); 
+      res.send(data); 
+    });
+});
+
+app.get('*.png', function (req, res) {
+    console.log(req);
+    fs.readFile('.' + req.originalUrl,function(err, data){  
+      res.set('Content-Type', 'image/png'); 
+      res.send(data); 
+    });
+});
+
